3b5f40c3de3b3221000206e047be386d6379fb03
Mirjam Ercegovac <mirjam.ercegovac@gmail.com>
2023-12-12 16:42:44 +0100

    Add media queries

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import './header.scss';
 import logo from "../../images/logo.png";
 import icon from "../../images/Vector.svg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <header className="header">
       <img src={logo} alt="logo" />
       <div className="vertical-line"></div>
 
-      <div className="nav">
+      <div className="hamburger" onClick={toggleNav}>
+        {isNavOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <div className={isNavOpen ? "nav active" : "nav"}>
         <nav className="navbar">
           <a href="">Services</a>
           <a href="">Projects</a>
@@ -25,7 +37,7 @@ const Header = () => {
             <img src={icon} alt=""/> Log in 
           </a>
           <a className="button-text">
-            <FontAwesomeIcon icon={faPaperPlane} style={{ color:  '#0052B4', width: '19px', height: '18.43px' }} /> Contact us
+            <FontAwesomeIcon className="font-icon" icon={faPaperPlane} style={{ color:  '#0052B4', width: '19px', height: '18.43px' }} /> Contact us
           </a>
           <a className="button">Sign up</a>
         </div>
